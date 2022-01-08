94db89548a843c5f10f4837aa9e27c2b9cebcc3e
jodiecottave <jodie.cottave@etu.umontpellier.fr>
2022-01-08 20:00:17 +0100

    edited classes files

--- src/app/shared/classes/step.ts
+++ src/app/shared/classes/step.ts
@@ -1,13 +1,12 @@
-import {StepComponent} from "./stepcomponent";
+
 import {Ingredient} from "./ingredient";
 
-export class Step implements StepComponent {
+export class Step {
   constructor(
     private step_id: number,
     private name: string,
     private description: string,
-    private duration: number,
-    private components: Map<StepComponent, number>
+    private duration: number
   ) {
   }
 
@@ -23,17 +22,7 @@ export class Step implements StepComponent {
     return this.duration;
   }
 
-  getIngredients(): Ingredient[] {
-    const allIngredients: Ingredient[] = []
-    for (const [key, value] of this.components) {
-      for (let i = 0; i < value; i++) {
-        for (const ingredient of key.getIngredients()) {
-          allIngredients.push(ingredient)
-        }
-      }
-    }
-    return allIngredients
-  }
+  
 
   getName(): string {
     return this.name;
